ae19b16b7aec745e963fb9926d958051bd25149f
Satoshi Watanabe <sassy.watanabe@gmail.com>
2017-01-11 23:51:16 +0900

    update chapter2

---
+++ chapter2/my_xor.py
@@ -0,0 +1,17 @@
+from my_and import AND
+from my_nand import NAND
+from my_or import OR
+
+def XOR(x1, x2):
+    s1 = NAND(x1, x2)
+    s2 = OR(x1, x2)
+    y = AND(s1, s2)
+    return y
+
+
+if __name__ == '__main__':
+    print("XOR")
+    print(XOR(0, 0))
+    print(XOR(0, 1))
+    print(XOR(1, 0))
+    print(XOR(1, 1))
